f9f5efc8e9f295e0c3ebeda1fc15db48e704efb8
Breno Zanchetta <bfzanchetta@hotmail.com>
2018-11-05 11:50:42 -0200

    Create tensorflow_install.sh

---
+++ jetsontx2/tensorflow_install.sh
@@ -0,0 +1,5 @@
+wget http://developer.download.nvidia.com/compute/redist/jp33/tensorflow-gpu/tensorflow_gpu-1.9.0+nv18.8-cp27-cp27mu-linux_aarch64.whl
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer -y
+sudo pip install tensorflow_gpu-1.9.0+nv18.8-cp27-cp27mu-linux_aarch64.whl
