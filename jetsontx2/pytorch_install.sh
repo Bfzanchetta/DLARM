98950ebc5c13931d1b05796a5778b65ac7821ce9
Breno Zanchetta <bfzanchetta@hotmail.com>
2018-11-05 11:38:00 -0200

    Update pytorch_install.sh

--- jetsontx2/pytorch_install.sh
+++ jetsontx2/pytorch_install.sh
@@ -67,6 +67,7 @@ sudo DEBUG=1 python setup.py develop
 
 sudo apt clean
 sudo apt-get install libjpeg-dev zlib1g-dev
+sudo pip install pandas
 git clone https://github.com/python-pillow/Pillow.git
 cd Pillow/
 sudo python setup.py install
