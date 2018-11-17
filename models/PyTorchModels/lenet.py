4006dff91b60f51f94755538a6624c41a6ba9d87
Breno Zanchetta <bfzanchetta@hotmail.com>
2018-11-17 04:12:40 -0200

    Update lenet.py

--- models/PyTorchModels/lenet.py
+++ models/PyTorchModels/lenet.py
@@ -1,4 +1,7 @@
-'''LeNet in PyTorch.'''
+###################################
+# LeNet implementation in PyTorch #
+# Adapted by Breno Zanchetta      #
+###################################
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -7,9 +10,9 @@ class LeNet(nn.Module):
         super(LeNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1   = nn.Linear(16*5*5, 120)
-        self.fc2   = nn.Linear(120, 84)
-        self.fc3   = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         out = F.relu(self.conv1(x))
