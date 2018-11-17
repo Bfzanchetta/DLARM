10977dd1bc19ef092607a5620b01b0a5ededdfd0
Breno Zanchetta <bfzanchetta@hotmail.com>
2018-11-17 03:33:20 -0200

    Create lenet_pt.py

---
+++ models/lenet_pt.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+from __future__ import division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+print("PyTorch Version: ",torch.__version__)
+print("Torchvision Version: ",torchvision.__version__)
