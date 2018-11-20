d37b5f7b9f4a21c471e5486e0b9e23ccf4cd9f7f
Breno Zanchetta <bfzanchetta@hotmail.com>
2018-11-20 20:11:43 -0200

    Create prepareImageNetDataset.sh

---
+++ jetsontx2/prepareImageNetDataset.sh
@@ -0,0 +1,5 @@
+#This code enters the dataset folder and uncompresses all .tar training archives into
+#individual folders with the same name as the tar file
+#
+#Author: Kassiano J. Matteussi
+for i in `ls`; do echo "Extracting $i ..."; aux=`echo $i| cut -d "." -f1`;mkdir $aux; tar -xvf $i -C $aux; done
