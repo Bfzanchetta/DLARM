c3ff928a395df15c957e6c635e3403a689e04756
Breno Zanchetta <bfzanchetta@hotmail.com>
2018-11-21 14:04:10 -0200

    Create download_imagenet.sh

---
+++ jetsontx2/download_imagenet.sh
@@ -0,0 +1,6 @@
+#Code made by Dusty-nv
+#https://github.com/dusty-nv/jetson-inference/blob/master/README.md
+wget --no-check-certificate https://nvidia.box.com/shared/static/gzr5iewf5aouhc5exhp3higw6lzhcysj.gz -O ilsvrc12_urls.tar.gz
+tar -xzvf ilsvrc12_urls.tar.gz
+wget https://rawgit.com/dusty-nv/jetson-inference/master/tools/imagenet-download.py
+python imagenet-download.py ilsvrc12_urls.txt . --jobs 100 --retry 3 --sleep 0
