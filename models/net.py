82c76516e2de505910a1751f3f629801061b9228
Breno Zanchetta <bfzanchetta@hotmail.com>
2018-11-30 11:07:41 -0200

    Update net.py

--- models/net.py
+++ models/net.py
@@ -5,6 +5,8 @@ import torch
 from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
 
 # Ignore warnings
 import warnings
@@ -28,3 +30,24 @@ dataset_loader = torch.utils.data.DataLoader(miniImageNet_dataset,
                                              num_workers=4)
 
 print("It Works")
+
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+return out
