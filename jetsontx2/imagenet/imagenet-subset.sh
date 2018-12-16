45cb71c70fe757ba4a2cb47cd8d981b74823855f
Breno Zanchetta <bfzanchetta@hotmail.com>
2018-12-16 21:59:52 -0200

    Update imagenet-subset.sh

--- jetsontx2/imagenet/imagenet-subset.sh
+++ jetsontx2/imagenet/imagenet-subset.sh
@@ -2,103 +2,85 @@
 #Number of files | Mean | Stddev
 # 1281167 | 114667 | 338.58972223031224194536
 mean=114667,24;
-media=0
-amountOfFiles=0;
 R=$((RANDOM%1000+1));
-stddev=0;
-acc=0;
+stddev=338.58972223031224194536
 N=12;
 outputpath="/home/breno/Desktop/train";
+balance=0;
 
-cd train/;
-for i in `ls`;
-        do cd $i;
-        for j in `ls`;
-                do std=$(stat -c '%s' "$j");
-                aux=$((std-(mean)));
-                acc=$(((acc)+((aux)^2)));
-                amountOfFiles=$((amountOfFiles+1));
-		media=$((media+(std)));
-                done;
-        cd ..; 
-        done;
-echo 'end for'
-acc=$((acc / (amountOfFiles)));
-stddev=$(echo "sqrt ( ($acc) )" | bc -l) ; 
-echo 'stddev'
-echo $stddev
-echo 'amount of files'
-echo $amountOfFiles
-echo 'media final'
-echo $((media/(amountOfFiles)))
+fourthLimit=115344,419444461;
+thirdLimit=115005,82972223;
+secondLimit=114328,65027777;
+firstLimit=113990,060555539;
 
 #lowerICNinetyFive=$((($mean)-1,64*(($stddev) / ($amountOfFiles))));
 #upperICNinetyFive=$((($mean)+1,64*(($stddev) / ($amountOfFiles))));
-
 #lowerICNinetyNine=$((($mean)-2,965*(($stddev) / ($amountOfFiles))));
 #upperICNinetyNine=$((($mean)+2,965*(($stddev) / ($amountOfFiles))));
-
 #Selects the random folders#
-
-
-for k in `ls`; do
-	cd $k;
-	a=( * )
-	randf=()
-	temp=0;
-	for((i=0;i<N && ${#a[@]};++i)); do
-    		((j=RANDOM%${#a[@]}))
-    		echo ${a[j]}
-		temp=$(stat -c '%s' "${a[j]}");
-		echo 'temp'
-		echo $temp
-		if [ $temp -gt 97503 ]; then
-			echo 'ulala'
-		else
-			echo 'notnotnotnotnotnot'
-		fi
-		done;
-	cd ..
-	done;
-echo 'fim do loop'	
-    #resto da logica de verificacao aqui
     
 #for i in `ls`; do
         #pegar a folder com o indice
 #        mkdir(outputpath/$i);
 #        done;
 
-localMedian=0;
-localStddev=0;
-balance=0;
+#localMedian=0;
+#localStddev=0;
 
-#for i in `ls`; do
-#        cd $i;
-#        a=( * )
-#        randf=()
-#        for((i=0;i<N && ${#a[@]};++i)); do
-#                ((j=RANDOM%${#a[@]}))
-#                if [ ${a[j]} -get $($(mean)-$(2*($(stddev)))) -a ${a[j]} -lt $($(mean)-$(stddev)) ]; then
-#                        $balance=$(balance+(mean-a[j]));
-#                elif [ ${a[j]} -get $(mean-stddev) -a ${a[j]} -lt $(mean) ]; then
-#                        $balance=$(balance+(mean-a[j]));
-#                elif [ ${a[j]} -get $(mean) -a ${a[j]} -lt $(mean+stddev) ]; then
-#                        if [ $balance -get  ${a[j]} ]; then
-#                                $balance=$(balance-(mean-a[j]));
-#                        else
-#                                #draw again
-#				echo 'oi'
-#                        fi
-#                elif [ ${a[j]} -get $(mean+stddev) -a ${a[j]} -lt $(mean+(2*stddev)) ]; then
-#                        if [ $balance -get  ${a[j]} ]; then
-#                                $balance=$(balance-(mean-a[j]));
-#                        else
-#                                #draw again
-#				echo 'draw again'
-#                        fi
-#                else
-#   			echo 'oi'
-#                fi
-#                done;
-#        cd ..;
-#        done;
+
+for i in `ls`; do
+        cd $i;
+        a=( * )
+        randf=()
+	fileSize=0;
+        for((i=0;i<N && ${#a[@]};++i)); do
+                ((j=RANDOM%${#a[@]}))
+		fileSize=$(stat -c '%s' "${a[j]}");
+		var=$(awk 'BEGIN{ print "'$fileSize'">="'$firstLimit'" }');
+		var1=$(awk 'BEGIN{ print "'$fileSize'">="'$secondLimit'" }');
+		var2=$(awk 'BEGIN{ print "'$fileSize'">="'$mean'" }');
+		var3=$(awk 'BEGIN{ print "'$fileSize'">="'$thirdLimit'" }');
+		var4=$(awk 'BEGIN{ print "'$fileSize'">="'$fourthLimit'" }');
+		var5=$(awk 'BEGIN{ print "'$fileSize'"<"'$firstLimit'" }');
+		var6=$(awk 'BEGIN{ print "'$fileSize'"<"'$secondLimit'" }');
+		var7=$(awk 'BEGIN{ print "'$fileSize'"<"'$mean'" }');
+		var8=$(awk 'BEGIN{ print "'$fileSize'"<"'$thirdLimit'" }');
+		var9=$(awk 'BEGIN{ print "'$fileSize'"<"'$fourthLimit'" }');
+		
+                if [ "$var" -eq 1 -a "$var6" -eq 1 ]; then
+                        $balance=$(balance+(mean-fileSize));
+                elif [ "$var1" -eq 1 -a "$var7" -eq 1 ]; then
+                        $balance=$(balance+(mean-fileSize));
+                elif [ "$var2" -eq 1 -a "$var8" -eq 1 ]; then
+                        if [ $balance -get  $fileSize ]; then
+                                $balance=$(balance-(mean-fileSize));
+                        else
+                                #draw again
+				echo 'oi'
+                        fi
+                elif [ "$var3" -eq 1 -a "$var9" -eq 1 ]; then
+                        if [ $balance -get  $fileSize ]; then
+                                $balance=$(balance-(mean-fileSize));
+                        else
+                                #draw again
+				echo 'draw again'
+                        fi
+                else
+   			echo 'oi'
+                fi
+                done;
+        cd ..;
+        done;
+
+
+key1=11.3
+result=12.5
+var=$(awk 'BEGIN{ print "'$key1'"<"'$result'" }')    
+# or var=$(awk -v key=$key1 -v result=$result 'BEGIN{print result<key?1:0}')
+# or var=$(awk 'BEGIN{print "'$result'"<"'$key1'"?1:0}')
+# or 
+if [ "$var" -eq 1 -a 3 -lt 5 ];then
+  echo "do something"
+else
+  echo "result more than key"
+fi
