e03b20bb7d29e90a1342ec7dbe06319b5f6030b9
Breno Zanchetta <bfzanchetta@hotmail.com>
2018-12-16 20:45:00 -0200

    Update stddev.sh

--- jetsontx2/imagenet/stddev.sh
+++ jetsontx2/imagenet/stddev.sh
@@ -1,29 +1,33 @@
+#!/bin/bash
+#Number of files | Mean | Stddev
+# 1281167 | 114667 | 338.58972223031224194536
 mean=114667,24;
+media=0
 amountOfFiles=0;
+R=$((RANDOM%1000+1));
+stddev=0;
 acc=0;
+N=12;
+outputpath="/home/breno/Desktop/train";
+
 cd train/;
 for i in `ls`;
         do cd $i;
         for j in `ls`;
                 do std=$(stat -c '%s' "$j");
-                aux=$(($std-($mean)));
-                acc=$((($acc)+(($aux)^2)));
-                amountOfFiles=$(($amountOfFiles+1));
+                aux=$((std-(mean)));
+                acc=$(((acc)+((aux)^2)));
+                amountOfFiles=$((amountOfFiles+1));
+		media=$((media+(std)));
                 done;
         cd ..; 
         done;
 echo 'end for'
-acc=$(($acc / ($amountOfFiles)));
+acc=$((acc / (amountOfFiles)));
 stddev=$(echo "sqrt ( ($acc) )" | bc -l) ; 
 echo 'stddev'
 echo $stddev
-
-#lowerICNinetyFive=$((($mean)-1,64*(($stddev) / ($amountOfFiles))));
-#upperICNinetyFive=$((($mean)+1,64*(($stddev) / ($amountOfFiles))));
-
-#lowerICNinetyNine=$((($mean)-2,965*(($stddev) / ($amountOfFiles))));
-#upperICNinetyNine=$((($mean)+2,965*(($stddev) / ($amountOfFiles))));
-
-random=$(echo "shuf -i1-1000 -n1");
-echo 'random eh:'
-echo $random
+echo 'amount of files'
+echo $amountOfFiles
+echo 'media final'
+echo $((media/(amountOfFiles)))
