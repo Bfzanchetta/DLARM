f1685bc183de66028ef1793831a307581341de6f
Breno Zanchetta <bfzanchetta@hotmail.com>
2018-12-17 13:36:45 -0200

    Update imagenet-subset.sh

--- jetsontx2/imagenet/imagenet-subset.sh
+++ jetsontx2/imagenet/imagenet-subset.sh
@@ -1,86 +1,110 @@
 #!/bin/bash
 #Number of files | Mean | Stddev
 # 1281167 | 114667 | 338.58972223031224194536
-mean=114667,24;
 R=$((RANDOM%1000+1));
-stddev=338.58972223031224194536
 N=12;
-outputpath="/home/breno/Desktop/train";
-balance=0;
+outputpath="/home/nvidia/dataset/train";
+soma=0;
+mean=114667.24;
+stddev=338.58972223031224194536;
+limit4=115344.419444461;
+limit3=115005.82972223;
+limit2=114328.65027777;
+limit1=113990.060555539;
 
-fourthLimit=115344,419444461;
-thirdLimit=115005,82972223;
-secondLimit=114328,65027777;
-firstLimit=113990,060555539;
+zona1=0;
+zona2=0;
+zona3=0;
+zona4=0;
+zona5=0;
+zona6=0;
+zona7=0;
 
-#lowerICNinetyFive=$((($mean)-1,64*(($stddev) / ($amountOfFiles))));
-#upperICNinetyFive=$((($mean)+1,64*(($stddev) / ($amountOfFiles))));
-#lowerICNinetyNine=$((($mean)-2,965*(($stddev) / ($amountOfFiles))));
-#upperICNinetyNine=$((($mean)+2,965*(($stddev) / ($amountOfFiles))));
-#Selects the random folders#
+balance=0.0;
     
 #for i in `ls`; do
-        #pegar a folder com o indice
 #        mkdir(outputpath/$i);
 #        done;
-
 #localMedian=0;
 #localStddev=0;
 
+#echo $limit4
+#echo $limit3
+#echo $limit2
+#echo $limit1
+#echo $mean
+#echo $fSize
+
+cd $outputpath;
 
 for i in `ls`; do
-        cd $i;
-        a=( * )
-        randf=()
-	fileSize=0;
-        for((i=0;i<N && ${#a[@]};++i)); do
-                ((j=RANDOM%${#a[@]}))
-		fileSize=$(stat -c '%s' "${a[j]}");
-		var=$(awk 'BEGIN{ print "'$fileSize'">="'$firstLimit'" }');
-		var1=$(awk 'BEGIN{ print "'$fileSize'">="'$secondLimit'" }');
-		var2=$(awk 'BEGIN{ print "'$fileSize'">="'$mean'" }');
-		var3=$(awk 'BEGIN{ print "'$fileSize'">="'$thirdLimit'" }');
-		var4=$(awk 'BEGIN{ print "'$fileSize'">="'$fourthLimit'" }');
-		var5=$(awk 'BEGIN{ print "'$fileSize'"<"'$firstLimit'" }');
-		var6=$(awk 'BEGIN{ print "'$fileSize'"<"'$secondLimit'" }');
-		var7=$(awk 'BEGIN{ print "'$fileSize'"<"'$mean'" }');
-		var8=$(awk 'BEGIN{ print "'$fileSize'"<"'$thirdLimit'" }');
-		var9=$(awk 'BEGIN{ print "'$fileSize'"<"'$fourthLimit'" }');
-		
-                if [ "$var" -eq 1 -a "$var6" -eq 1 ]; then
-                        $balance=$(balance+(mean-fileSize));
-                elif [ "$var1" -eq 1 -a "$var7" -eq 1 ]; then
-                        $balance=$(balance+(mean-fileSize));
-                elif [ "$var2" -eq 1 -a "$var8" -eq 1 ]; then
-                        if [ $balance -get  $fileSize ]; then
-                                $balance=$(balance-(mean-fileSize));
-                        else
-                                #draw again
+	cd $i;
+	a=( * )
+	randf=()
+	fSize=0;
+	for((i=0;i<N && ${#a[@]};++i)); do
+		((j=RANDOM%${#a[@]}))
+		fSize=$(stat -c '%s' "${a[j]}");
+		if [ `echo $fSize'<'$limit1 | bc -l` == 1 ]; then
+			echo 'arquivo pequeno'
+			zona1=$((zona1+1));
+		elif [ `echo $fSize'>='$limit1 | bc -l` == 1 -a `echo $fSize'<'$limit2 | bc -l` == 1 ]; then
+            		soma=`echo "$mean - $fSize" | bc`;
+			balance=`echo "$balance + $soma" | bc`;
+			zona2=$((zona2+1));
+			echo 'arquivo 1'
+        	elif [ `echo $fSize'>='$limit2 | bc -l` == 1 -a `echo $fSize'<'$mean | bc -l` == 1 ]; then
+            		soma=`echo "$mean - $fSize" | bc`;
+                        balance=`echo "$balance + $soma" | bc`;
+			zona3=$((zona3+1));
+			echo 'arquivo 2'
+        	elif [ `echo $fSize'=='$mean | bc -l` == 1 ]; then
+			echo "Sera que isso acontece?"
+			zona4=$((zona4+1));
+        	elif [ `echo $fSize'>'$mean | bc -l` == 1 -a `echo $fSize'<='$limit3 | bc -l` == 1 ]; then
+			echo 'arquivo 3'
+			zona5=$((zona5+1));
+			if [ `echo $balance'>'$fSize | bc -l` == 1 ]; then
+				soma=`echo "$fSize - $mean" | bc`;
+                		balance=`echo "$balance + $soma" | bc`;
+            		else
+                		#draw again
 				echo 'oi'
-                        fi
-                elif [ "$var3" -eq 1 -a "$var9" -eq 1 ]; then
-                        if [ $balance -get  $fileSize ]; then
-                                $balance=$(balance-(mean-fileSize));
-                        else
-                                #draw again
+            		fi
+        	elif [ `echo $fSize'>'$limit3 | bc -l` == 1 -a `echo $fSize'<='$limit4 | bc -l` == 1 ]; then
+			echo 'arquivo 4'
+			zona6=$((zona6+1));
+			if [ `echo $balance'>'$fSize | bc -l` == 1 ]; then
+                		soma=`echo "$fSize - $mean" | bc`;
+                                balance=`echo "$balance + $soma" | bc`;
+            		else
+                		#draw again
 				echo 'draw again'
-                        fi
-                else
-   			echo 'oi'
-                fi
-                done;
-        cd ..;
-        done;
-
-
-key1=11.3
-result=12.5
-var=$(awk 'BEGIN{ print "'$key1'"<"'$result'" }')    
-# or var=$(awk -v key=$key1 -v result=$result 'BEGIN{print result<key?1:0}')
-# or var=$(awk 'BEGIN{print "'$result'"<"'$key1'"?1:0}')
-# or 
-if [ "$var" -eq 1 -a 3 -lt 5 ];then
-  echo "do something"
-else
-  echo "result more than key"
-fi
+            		fi
+		elif [ `echo $fSize'>'$limit4 | bc -l` == 1 ]; then
+			echo 'arquivo grande'
+			zona7=$((zona7+1));
+        	else
+	    		echo '**************'
+   	    		echo '     Erro     '
+	    		echo '**************'
+        	fi
+        	done;
+	cd ..;
+    	done;
+echo 'Resultados'
+echo 'zona1'
+echo $zona1
+echo 'zona2'
+echo $zona2
+echo 'zona3'
+echo $zona3
+echo 'zona4'
+echo $zona4
+echo 'zona5'
+echo $zona5
+echo 'zona6'
+echo $zona6
+echo 'zona7'
+echo $zona7
+echo 'Fim'
