adc37d8916931b9968974e64b1a3aa59df6b1db8
Breno Zanchetta <bfzanchetta@hotmail.com>
2018-12-22 16:55:31 -0200

    Update tensorflow_install.sh

--- jetsontx2/tensorflow_install.sh
+++ jetsontx2/tensorflow_install.sh
@@ -3,3 +3,4 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer -y
 sudo pip install tensorflow_gpu-1.9.0+nv18.8-cp27-cp27mu-linux_aarch64.whl
+sudo pip install tensorflow-datasets
