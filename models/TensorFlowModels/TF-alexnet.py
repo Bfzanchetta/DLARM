f4d3918751a52a62640d2071ded5a5e64193cac5
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-01-07 19:59:54 -0200

    Update TF-alexnet.py

--- models/TensorFlowModels/TF-alexnet.py
+++ models/TensorFlowModels/TF-alexnet.py
@@ -16,4 +16,23 @@ import sys
 import warnings
 warnings.filterwarnings("ignore")
 
+epochs = 10
+
+#https://www.tensorflow.org/guide/datasets
+def _parse_function(filename, label):
+  image_string = tf.read_file(filename)
+  image_decoded = tf.image.decode_jpeg(image_string)
+  image_resized = tf.image.resize_images(image_decoded, [28, 28])
+  return image_resized, label
+
+# A vector of filenames.
+filenames = tf.constant(["/var/data/image1.jpg", "/var/data/image2.jpg", ...])
+
+# `labels[i]` is the label for the image in `filenames[i].
+labels = tf.constant([0, 37, ...])
+
+dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
+dataset = dataset.map(_parse_function)
+
+#to be continued
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
