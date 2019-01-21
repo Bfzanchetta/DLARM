4a569946328dc2ad8b3d8345fa6c0a950606d975
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-01-21 14:36:55 -0200

    Update prepareImageNetDataset.sh

--- jetsontx2/imagenet/prepareImageNetDataset.sh
+++ jetsontx2/imagenet/prepareImageNetDataset.sh
@@ -2,4 +2,4 @@
 #individual folders with the same name as the tar file
 #
 #Author: Kassiano J. Matteussi
-for i in `ls`; do echo "Extracting $i ..."; aux=`echo $i| cut -d "." -f1`;mkdir $aux; tar -xvf $i -C $aux; done
+for i in `ls`; do echo "Extracting $i ..."; aux=`echo $i| cut -d "." -f1`;mkdir $aux; tar -xvf $i -C $aux; rm $i; done
