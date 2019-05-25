d53365b4e4eacfffffa33daf817ceae0480275c2
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-05-25 14:57:48 -0300

    Create mxnet_install.sh

---
+++ jetsontx2/mxnet_install.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install -y git build-essential libatlas-base-dev libopencv-dev graphviz python-pip
+sudo pip install --upgrade pip
+sudo pip install --upgrade setuptools
+#good practice
+sudo pip install protobuf
+sudo apt-get install libboost-dev libboost-all-dev
+#end parenthesis
+sudo pip install numpy==1.15.2
+sudo pip install graphviz jupyter
+git clone https://github.com/apache/incubator-mxnet.git --branch v1.4.x --recursive
+cd incubator-mxnet/
