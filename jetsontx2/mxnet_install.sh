1df270b6dc1307248371cd935a2b81c7a36c7299
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-05-27 16:26:46 -0300

    Update mxnet_install.sh

--- jetsontx2/mxnet_install.sh
+++ jetsontx2/mxnet_install.sh
@@ -7,12 +7,22 @@ sudo apt install liblapacke-dev checkinstall # For OpenCV
 sudo pip install --upgrade pip==9.0.1
 sudo pip install numpy==1.16.2
 
-sudo pip install --upgrade setuptools
+sudo pip install scipy # ~20-30 min
+sudo apt-get install python-matplotlib
+sudo pip install matplotlib==2.2.3
+sudo pip install pyyaml
+sudo pip install scikit-build
+sudo apt-get -y install cmake
+sudo apt install libffi-dev
+sudo pip install cffi
+sudo pip install pandas # ~20-30 min
+sudo pip install Cython
+sudo pip install scikit-image
+sudo apt install python-sklearn 
 #good practice
 sudo pip install protobuf
 sudo apt-get install libboost-dev libboost-all-dev
 #end parenthesis
-sudo pip install numpy==1.15.2
 sudo pip install graphviz jupyter
-git clone https://github.com/apache/incubator-mxnet.git --branch v1.4.x --recursive
+#git clone https://github.com/apache/incubator-mxnet.git --branch v1.4.x --recursive
 cd incubator-mxnet/
