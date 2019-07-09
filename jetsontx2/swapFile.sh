77c74c8a4de682f3db3f06c1460809dde1e4fce4
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-07-09 14:53:25 -0300

    Update swapFile.sh

--- jetsontx2/swapFile.sh
+++ jetsontx2/swapFile.sh
@@ -4,7 +4,8 @@ chmod 600 swapfile
 ls -lh swapfile
 mkswap swapfile
 sudo swapon swapfile
-swapon -s
-sudo gedit /etc/fstab
-    > swapfile_adress(including the filename)	none	swap   	sw    0    0
+whereIsSwap=`swapon -s`
+swap=`echo $whereIsSwap| cut -d " " -f 6`;
+echo $swap"     none""     swap""     sw""     0""     0" >> /etc/fstab
+    # swapfile_adress(including the filename)	none	swap   	sw    0    0
 reboot
