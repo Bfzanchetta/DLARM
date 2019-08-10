809cf4bdf05980cf640fcfe641f75acd99121e20
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-08-10 14:54:48 -0300

    Update unified.sh

--- jetsontx2/Install/unified.sh
+++ jetsontx2/Install/unified.sh
@@ -3,3 +3,5 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer -y
 sudo apt-get install openjdk-8-jdk maven
+
+apt-get install autoconf automake libtool curl make g++ unzip
