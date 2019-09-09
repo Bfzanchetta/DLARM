ace19ad85f317bb90e08917b0f30d7e0af53e7b5
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-09-09 12:06:31 -0300

    Update unified.sh

--- jetsontx2/Install/unified.sh
+++ jetsontx2/Install/unified.sh
@@ -13,19 +13,11 @@ sudo apt-get install autoconf automake libtool curl make g++ unzip apt-utils git
 sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-dev libhdf5-serial-dev protobuf-compiler libprotobuf-java
 sudo apt-get install --no-install-recommends libboost-all-dev
 sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev
-sudo pip install protobuf numpy
-
+sudo pip install protobuf numpy# ~10-20 min on HDD
+sudo apt-get install libfreetype6-dev pkg-config libpng-dev
 sudo apt-get install gfortran
-sudo pip install scipy # ~20-30 min on HDD
-sudo apt-get install python-matplotlib
-sudo apt-get install libcanberra-gtk-module
-apt-get install libfreetype6-dev pkg-config libpng-dev
-
-
-
-
-
-
+sudo pip install scipy# ~20-30 min on HDD
+sudo apt-get install -y python-matplotlib libcanberra-gtk-module
 
 
 echo "export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:/usr/local/lib/python2.7/:$LD_LIBRARY_PATH" >> ~/.bashrc
