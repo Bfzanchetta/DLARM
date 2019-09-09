2720b98f5a5d59df93a4214adfd15330285a9fb3
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-09-09 12:57:36 -0300

    Update unified.sh

--- jetsontx2/Install/unified.sh
+++ jetsontx2/Install/unified.sh
@@ -55,7 +55,7 @@ DIR="/sys/devices/system/cpu/cpu5/"
   sed -i 's/USE_CUDA_PATH = NONE/USE_CUDA_PATH = \/usr\/local\/cuda/' config.mk
   sed -i 's/USE_CUDNN = 0/USE_CUDNN = 1/' config.mk
   sed -i '/USE_CUDNN/a CUDA_ARCH := -gencode arch=compute_53,code=sm_53 -gencode arch=compute_62,code=sm_62' config.mk
-  
+  sed -i 's/USE_MKLDNN =/USE_MKLDNN = 0/' config.mk
   sudo make -j 4
   #Install PyTorch
   export USE_NCCL=0
