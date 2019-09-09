0844a71063001e7592328ab9c23f040af4f876f3
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-09-09 14:20:50 -0300

    Update hadoop.sh

--- jetsontx2/Install/hadoop.sh
+++ jetsontx2/Install/hadoop.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 #Author: Breno Zanchetta
 #Based on Scripts of: Jorge Ximendes
+#User needs to start the download of Java at Oracle, which requires Login
+#alternatively, can use this:
+wget https://github.com/frekele/oracle-java/releases/download/8u212-b10/jdk-8u212-linux-arm64-vfp-hflt.tar.gz
+tar -xf jdk-8u212-linux-arm64-vfp-hflt.tar.gz
+cd jdk1.8.0_212/
+cd bin/
+echo "export JAVA_HOME="`pwd` >> ~/.bashrc
+source ~/.bashrc
 
