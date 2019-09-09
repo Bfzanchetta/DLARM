895f794e3e28ed6b5c2f9753e5fe5d00fd0a8f8e
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-09-09 12:04:14 -0300

    Update unified.sh

--- jetsontx2/Install/unified.sh
+++ jetsontx2/Install/unified.sh
@@ -6,12 +6,27 @@ sudo apt-get update
 sudo apt-get install oracle-java8-installer -y
 sudo apt-get install openjdk-8-jdk maven
 
-apt-get install autoconf automake libtool curl make g++ unzip apt-utils
-apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-dev libhdf5-serial-dev protobuf-compiler libprotobuf-java
-apt-get install --no-install-recommends libboost-all-dev
-apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
-apt-get install libatlas-base-dev libopenblas-dev
-pip install protobuf
+#resto dos pacotes
+sudo apt install -y liblapack3 libopenblas-base libopenblas-dev libatlas-dev libatlas-base-dev 
+sudo apt install -y liblapacke-dev checkinstall # For OpenCV
+sudo apt-get install autoconf automake libtool curl make g++ unzip apt-utils git build-essential libopencv-dev graphviz python-pip
+sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-dev libhdf5-serial-dev protobuf-compiler libprotobuf-java
+sudo apt-get install --no-install-recommends libboost-all-dev
+sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev
+sudo pip install protobuf numpy
+
+sudo apt-get install gfortran
+sudo pip install scipy # ~20-30 min on HDD
+sudo apt-get install python-matplotlib
+sudo apt-get install libcanberra-gtk-module
+apt-get install libfreetype6-dev pkg-config libpng-dev
+
+
+
+
+
+
+
 
 echo "export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:/usr/local/lib/python2.7/:$LD_LIBRARY_PATH" >> ~/.bashrc
 source ~/.bashrc
