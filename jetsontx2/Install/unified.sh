9370ead1036fb1736b21c07d305ce5b935bd0d6e
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-09-09 12:24:07 -0300

    Update unified.sh

--- jetsontx2/Install/unified.sh
+++ jetsontx2/Install/unified.sh
@@ -10,38 +10,19 @@ sudo apt-get install openjdk-8-jdk maven
 #resto dos pacotes
 sudo apt install -y liblapack3 libopenblas-base libopenblas-dev libatlas-dev libatlas-base-dev liblapack-dev
 sudo apt install -y liblapacke-dev checkinstall # For OpenCV
-sudo apt-get install autoconf automake libtool curl make g++ unzip apt-utils git alien nano build-essential python-dev libopencv-dev graphviz python-pip
+sudo apt-get install autoconf automake libtool curl make cmake g++ unzip apt-utils git alien nano build-essential python-dev libopencv-dev graphviz python-pip
 sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-dev libhdf5-serial-dev protobuf-compiler libprotobuf-java
-sudo apt-get install --no-install-recommends libboost-all-dev
+sudo apt-get install --no-install-recommends libboost-all-dev libboost-dev
 sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev
 sudo pip install protobuf numpy scipy# ~20-40 min on HDD
 sudo apt-get install -y libfreetype6-dev pkg-config libpng-dev libjpeg-dev zlib1g-dev
 sudo apt-get install gfortran
 sudo apt-get install -y python-matplotlib libcanberra-gtk-module
 sudo pip install matplotlib==2.2.3
-
-#mxnet nao organizado 
-
-sudo pip install pyyaml
-sudo pip install scikit-build
-sudo apt-get -y install cmake
-sudo apt install libffi-dev
-sudo pip install cffi
-sudo pip install pandas # ~20-30 min on HDD
-sudo pip install Cython
-sudo pip install scikit-image
-sudo apt install python-sklearn 
-sudo pip install protobuf
-sudo apt-get install libboost-dev libboost-all-dev
-sudo pip install graphviz jupyter
-
-#pytorch nao organizado
-
-sudo pip install pyyaml
-sudo pip install scikit-build
-sudo apt-get -y install cmake
+sudo pip install pyyaml scikit-build jupyter
 sudo apt install -y libffi-dev
-sudo pip install cffi
+sudo pip install cffi pandas Cython scikit-image python-sklearn 
+#se protobuf falhar, jogar pra ca
 
 echo "export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:/usr/local/lib/python2.7/:$LD_LIBRARY_PATH" >> ~/.bashrc
 source ~/.bashrc
