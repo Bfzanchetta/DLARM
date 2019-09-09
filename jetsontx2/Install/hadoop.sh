28b8cd43a882f7c4f847f3bd6fe0ce634904738c
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-09-09 14:22:37 -0300

    Update hadoop.sh

--- jetsontx2/Install/hadoop.sh
+++ jetsontx2/Install/hadoop.sh
@@ -9,4 +9,9 @@ cd jdk1.8.0_212/
 cd bin/
 echo "export JAVA_HOME="`pwd` >> ~/.bashrc
 source ~/.bashrc
-
+cd
+sudo apt-get install -y openssh* openssl* libssl* pkg-config* cmake* libsnappy-dev* bzip2* libbz2-dev* build-essential* autoconf* automake* libtool* zlib1g* libjansson* fuse*
+wget https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz
+tar xzvf protobuf-2.5.0.tar.gz
+cd protobuf-2.5.0/
+./configure --prefix=/usr
