e0e649653021e60ea7f074797ba7134672924d59
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-09-09 22:57:03 -0300

    Update hadoop.sh

--- jetsontx2/Install/hadoop.sh
+++ jetsontx2/Install/hadoop.sh
@@ -3,13 +3,15 @@
 #Based on Scripts of: Jorge Ximendes
 #User needs to start the download of Java at Oracle, which requires Login, I suggest 8u221
 #alternatively, can use this:
+sudo apt-get update && apt-get remove openjdk*
 wget https://github.com/frekele/oracle-java/releases/download/8u212-b10/jdk-8u212-linux-arm64-vfp-hflt.tar.gz
-tar -xf jdk-8u212-linux-arm64-vfp-hflt.tar.gz
-cd jdk1.8.0_212/
-cd bin/
-echo "export JAVA_HOME="`pwd` >> ~/.bashrc
-source ~/.bashrc
-cd
+
+tar -zxvf jdk-8u112-linux-arm64-vfp-hflt.tar.gz
+sudo mkdir -p /opt/java
+sudo mv jdk1.8.0_112 /opt/java
+sudo update-alternatives --install "/usr/bin/java" "java" "/opt/java/jdk1.8.0_112/bin/java" 1
+sudo update-alternatives --set java /opt/java/jdk1.8.0_112/bin/java
+
 
 wget https://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 tar -xf apache-maven-3.3.9-bin.tar.gz
