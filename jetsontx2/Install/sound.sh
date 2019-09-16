4a059218ec9d6d9a127b2778e408b1da3c74b41c
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-09-16 17:11:21 -0300

    Update sound.sh

--- jetsontx2/Install/sound.sh
+++ jetsontx2/Install/sound.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y libsdl2-2.0-0 ffmpeg-doc ffmpeg libavdevice57 libsndfile1
+sudo apt-get install -y libsdl2-2.0-0 ffmpeg-doc ffmpeg libavdevice57 libsndfile1 libedit-dev
 sudo apt-get install -y libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 sudo pip install Cython cffi PySoundFile
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-7.1.0/llvm-7.1.0.src.tar.xz
