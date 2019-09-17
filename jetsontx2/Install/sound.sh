218eb8eb083b2b8d4f6803a3c235f9c59d871a6c
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-09-16 23:12:43 -0300

    Update sound.sh

--- jetsontx2/Install/sound.sh
+++ jetsontx2/Install/sound.sh
@@ -12,6 +12,7 @@ cd
 cd Downloads
 git clone https://github.com/numba/llvmlite.git
 sudo LLVM_CONFIG=/home/dlarm/Downloads/llvm-7.1.0.src/build/bin/llvm-config python setup.py develop
+sudo LLVM_CONFIG=/home/dlarm/Downloads/llvm-7.1.0.src/build/bin/llvm-config python setup.py install
 http://llvmlite.pydata.org/en/latest/admin-guide/install.html
 http://llvmlite.pydata.org/en/latest/
 https://pypi.org/project/llvmlite/
