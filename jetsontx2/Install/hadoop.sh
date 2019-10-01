500205c34c73aa5dc243b86ed1710665bc1223c3
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-10-01 15:01:22 -0300

    Update hadoop.sh

--- jetsontx2/Install/hadoop.sh
+++ jetsontx2/Install/hadoop.sh
@@ -8,7 +8,7 @@ source ~/.bashrc
 sudo apt-get update
 sudo apt-get remove openjdk*
 
-cd BigData
+cd $BigData
 wget https://download.oracle.com/otn/java/jdk/8u112-b15/jdk-8u112-linux-arm64-vfp-hflt.tar.gz
 
 tar -zxvf jdk-8u112-linux-arm64-vfp-hflt.tar.gz
@@ -20,7 +20,7 @@ sudo update-alternatives --set java /opt/java/jdk1.8.0_112/bin/java
 sudo apt install python python-dev rpm yum build-essential libfreetype6 libfreetype6-dev fontconfig fontconfig-config libfontconfig1-dev libssl-dev openssl findbugs -y
 sudo apt-get install -y openssh* openssl* libssl* pkg-config* cmake* libsnappy-dev* bzip2* libbz2-dev* build-essential* autoconf* automake* libtool* zlib1g* libjansson* fuse*
 
-cd BigData
+cd $BigData
 wget https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz
 tar xzvf protobuf-2.5.0.tar.gz
 cd protobuf-2.5.0/
@@ -36,14 +36,14 @@ make
 make check
 sudo make install
 
-cd BigData
+cd $BigData
 wget https://archive.apache.org/dist/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz
 tar xvf apache-maven-3.0.5-bin.tar.gz
 cd apache-maven-3.0.5/bin
 echo "export PATH=$PWD:$PATH" >> ~/.bashrc
 source ~/.bashrc
 
-cd BigData
+cd $BigData
 wget https://downloads.lightbend.com/scala/2.11.6/scala-2.11.6.tgz
 tar -xf scala-2.11.6.tgz 
 
