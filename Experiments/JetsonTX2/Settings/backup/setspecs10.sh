635458887e8ee908fb7ff6a91f1c3b68e8237edd
bfzanchetta <bfzanchetta@hotmail.com>
2019-10-09 15:54:14 -0300

    Commit 09-10-2019-15:54

---
+++ Experiments/JetsonTX2/Settings/backup/setspecs10.sh
@@ -0,0 +1,8 @@
+#arm+ 1cpu +2 Denver
+sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+sudo sh -c "echo 1 > /sys/devices/system/cpu/cpu0/online"
+sudo sh -c "echo 1 > /sys/devices/system/cpu/cpu1/online"
+sudo sh -c "echo 1 > /sys/devices/system/cpu/cpu2/online"
+sudo sh -c "echo 0 > /sys/devices/system/cpu/cpu3/online"
+sudo sh -c "echo 0 > /sys/devices/system/cpu/cpu4/online"
+sudo sh -c "echo 1 > /sys/devices/system/cpu/cpu5/online"
