c966ad519832e82ff0df828540f576ddb1efa8bf
Breno Zanchetta <bfzanchetta@hotmail.com>
2019-10-26 16:08:09 -0300

    Update processMxNet.sh

--- Utils/processMxNet.sh
+++ Utils/processMxNet.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 arquivo=file.txt
-for i in `cat $arquivo`; do
-  
-  done;
+while IFS= read -r line
+do
+  train=echo "$line" | cut -d "," -f 2
+  time=echo "$line" | cut -d "]" -f 2
+done < "$arquivo"
