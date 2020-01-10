e149a99feda9a547cff355b986900ae759c040ea
Breno Zanchetta <bfzanchetta@hotmail.com>
2020-01-10 18:23:42 -0300

    Update swapFile.sh

--- Utils/swapFile.sh
+++ Utils/swapFile.sh
@@ -1,3 +1,12 @@
+1) Apagar tudo pela aplicação Disks do Ubuntu.
+2) Usar o comando $sudo fdisk -l  para descobrir a partição
+3) $sudo mkfs.ext4 /dev/sdb1
+4) Criar um diretório onde quer montar o disco: $mkdir tantofaz/
+5) $chmod 757 tantofaz/
+6) $sudo mount /dev/sdb1 tantofaz/
+7) $sudo cp -ax <pasta de origem> <pasta de destino>
+8) $sudo umount /dev/sdb1
+
 fallocate -l 8G swapfile
 ls -lh swapfile
 chmod 600 swapfile
